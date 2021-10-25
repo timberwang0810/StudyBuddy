62539de9f24eaef6ddfc6315bdedad4d469f8e15
timberwang0810 <zilinwan@andrew.cmu.edu>
2021-10-25 15:12:25 -0400

    initial xcode proj setup

---
+++ StudyBuddyApp/StudyBuddyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudyBuddyApp
+//
+//  Created by Tim Wang on 10/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
