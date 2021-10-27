580f9517acb58fa2cedd2d3097a4ddbdea40e3d9
Austin Leung <austinle@andrew.cmu.edu>
2021-10-26 23:48:46 -0400

    Initialize view model

---
+++ StudyBuddyApp/StudyBuddyApp/ViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ViewModel.swift
+//  StudyBuddyApp
+//
+//  Created by austin on 10/26/21.
+//
+
+import SwiftUI
+
+// Contains model logic we want to access from views through published variables.
+// Write functions in here to be called that change models (e.g. when a button is clicked)
+struct ViewModel: ObservableObject {
+//  State we want to update view with when it changes
+//  @Published var urlString: String = ""
+  
+}
