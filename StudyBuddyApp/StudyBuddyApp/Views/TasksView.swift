dd984037be915b1ed8f42f45f027d8eda6ae67ee
Austin Leung <austinle@andrew.cmu.edu>
2021-10-31 00:36:38 -0400

    Add background and card

--- StudyBuddyApp/StudyBuddyApp/Views/TasksView.swift
+++ StudyBuddyApp/StudyBuddyApp/Views/TasksView.swift
@@ -15,78 +15,88 @@ struct TasksView: View {
     @State private var selection: TaskCategory = .STUDY
     
     var body: some View {
-        VStack {
-            
-            VStack(alignment: .leading) {
-                Text("New Task")
-                    .font(Font.custom("Chalkboard SE", size: 24))
-                    .padding();
+        ZStack {
+            Color(red: 241 / 255, green: 241 / 255, blue: 241 / 255).edgesIgnoringSafeArea([.top])
+            VStack {
                 
-                HStack {
-                    Text("Name")
-                        .fontWeight(.light)
-                    TextField("Task Name", text: $name)
-                        .padding()
-                        .cornerRadius(3.0);
-                }
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-                
-                VStack(alignment: .leading, spacing: -15) {
-                    Text("Duration")
-                        .fontWeight(.light)
-                    DurationPicker(duration: $duration)
-                        .padding(.horizontal)
-                }.padding(.horizontal)
-                
-                HStack {
-                    Text("Category")
-                        .fontWeight(.light)
-                    Picker("\(selection.rawValue)", selection: $selection) {
-                        ForEach(TaskCategory.allCases.reversed(), id: \.self) {
-                            Text($0.rawValue)
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                }.padding(.horizontal)
-            }
-            .font(Font.custom("Chalkboard SE", size: 18))
-            .padding(20)
-            
-            VStack(alignment: .center) {
-                VStack{
-                    Text("Reward")
-                        .fontWeight(.light)
+                VStack(alignment: .leading) {
+                    Text("New Task")
+                        .font(Font.custom("Chalkboard SE", size: 24))
+                        .padding();
+                    
                     HStack {
-                        Image("coin")
-                        Text("\(Task.calculateBaseRewards(duration: duration))+")
-                            .font(Font.custom("Chalkboard SE", size: 34))
+                        Text("Name")
                             .fontWeight(.light)
+                        TextField("Task Name", text: $name)
+                            .padding()
+                            .cornerRadius(3.0);
                     }
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding(.horizontal)
+                    
+                    VStack(alignment: .leading, spacing: -15) {
+                        Text("Duration")
+                            .fontWeight(.light)
+                        DurationPicker(duration: $duration)
+                            .padding(.horizontal)
+                    }.padding(.horizontal)
+                    
+                    HStack {
+                        Text("Category")
+                            .fontWeight(.light)
+                        Picker("\(selection.rawValue)", selection: $selection) {
+                            ForEach(TaskCategory.allCases.reversed(), id: \.self) {
+                                Text($0.rawValue)
+                            }
+                        }
+                        .pickerStyle(MenuPickerStyle())
+                    }.padding(.horizontal)
                 }
-                .padding()
-                .padding(.horizontal, 40)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color(red: 230 / 255, green: 230 / 255, blue: 230 / 255), lineWidth: 2)
-                )
+                .font(Font.custom("Chalkboard SE", size: 18))
+                .padding(20)
+                
+                VStack(alignment: .center) {
+                    VStack{
+                        Text("Reward")
+                            .fontWeight(.light)
+                        HStack {
+                            Image("coin")
+                            Text("\(Task.calculateBaseRewards(duration: duration))+")
+                                .font(Font.custom("Chalkboard SE", size: 34))
+                                .fontWeight(.light)
+                        }
+                        .padding(.horizontal)
+                    }
+                    .padding()
+                    .padding(.horizontal, 40)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color(red: 230 / 255, green: 230 / 255, blue: 230 / 255), lineWidth: 2)
+                    )
+                }
+                .padding(20)
+                
+                Button(action: {self.viewModel.createTask(name: name, duration: duration, category: selection, isStarted: true, completion: {
+                    viewRouter.currentPage = .doingTaskPage
+                })}) {
+                    Text("Start Now")
+                        .font(Font.custom("Chalkboard SE", size: 20))
+                        .fontWeight(.light)
+                }
+                    .padding()
+                    .background(Color(red: 248 / 255, green: 208 / 255, blue: 116 / 255))
+                    .foregroundColor(.black)
+                    .cornerRadius(10)
+                    .shadow(color: Color(red: 185 / 255, green: 108 / 255, blue: 37 / 255), radius: 1, x: 0, y: 5)
+                Spacer()
             }
-            .padding(20)
-            
-            Button(action: {self.viewModel.createTask(name: name, duration: duration, category: selection, isStarted: true, completion: {
-                viewRouter.currentPage = .doingTaskPage
-            })}) {
-                Text("Start Now")
-                    .font(Font.custom("Chalkboard SE", size: 20))
-                    .fontWeight(.light)
-            }
-                .padding()
-                .background(Color(red: 248 / 255, green: 208 / 255, blue: 116 / 255))
-                .foregroundColor(.black)
-                .cornerRadius(10)
-                .shadow(color: Color(red: 185 / 255, green: 108 / 255, blue: 37 / 255), radius: 1, x: 0, y: 5)
-        }.font(Font.custom("Chalkboard SE", size: 22))
+            .font(Font.custom("Chalkboard SE", size: 22))
+            .frame(maxWidth: 385, maxHeight: 725)
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(color: Color.gray, radius: 3, x: 0, y: 5)
+        }
+        
     }
 }
 
