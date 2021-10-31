a19fe53866048b413344b43abf6b437f756d7d22
timberwang0810 <zilinwan@andrew.cmu.edu>
2021-10-31 14:15:03 -0400

    added item tests

--- StudyBuddyApp/StudyBuddyApp/Models/Item.swift
+++ StudyBuddyApp/StudyBuddyApp/Models/Item.swift
@@ -13,12 +13,31 @@ protocol Item{
   var price: Int {get set}
 }
 
-extension Item{
-  var name: String {get {"Name"} set{self.name = newValue}}
-  var price: Int {get{100} set{self.price = newValue}}
-  
-  static func == (lhs: Item, rhs: Item) -> Bool {
-      return lhs.name == rhs.name && lhs.price == rhs.price
+//extension Item{
+//
+//  static func == (lhs: Item, rhs: Item) -> Bool {
+//    print(lhs.name)
+//    print(rhs.name)
+//    return lhs.name == rhs.name
+//  }
+//
+//  func hash(into hasher: inout Hasher) {
+//      hasher.combine(name)
+//      hasher.combine(price)
+//  }
+//}
+
+struct AccessoryItem : Item, Equatable, Hashable{
+  var name: String
+  var price: Int
+  var category : AccessoryItemCategory
+  init(name: String, price: Int, category: AccessoryItemCategory){
+    self.category = category
+    self.name = name
+    self.price = price
+  }
+  static func == (lhs: AccessoryItem, rhs: AccessoryItem) -> Bool {
+    return lhs.name == rhs.name
   }
 
   func hash(into hasher: inout Hasher) {
@@ -27,27 +46,21 @@ extension Item{
   }
 }
 
-struct AccessoryItem : Item, Equatable, Hashable{
-  var category : AccessoryItemCategory
-  init(name: String, price: Int, category: AccessoryItemCategory){
-    self.category = category
-    self.name = name
-    self.price = price
-  }
-  
-  static func == (lhs: AccessoryItem, rhs: AccessoryItem) -> Bool{
-    return lhs.name == rhs.name
-  }
-}
-
 struct PlaygroundItem : Item, Equatable, Hashable{
+  var name: String
+  var price: Int
   var category : PlaygroundItemCategory
   init(name: String, price: Int, category: PlaygroundItemCategory){
     self.category = category
     self.name = name
     self.price = price
   }
-  static func == (lhs: PlaygroundItem, rhs: PlaygroundItem) -> Bool{
+  static func == (lhs: PlaygroundItem, rhs: PlaygroundItem) -> Bool {
     return lhs.name == rhs.name
+  }
+
+  func hash(into hasher: inout Hasher) {
+      hasher.combine(name)
+      hasher.combine(price)
   }
 }
