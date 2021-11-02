7c04f85ae9390517b2b57d161fe82f3698459509
Austin Leung <austinle@andrew.cmu.edu>
2021-11-02 01:12:35 -0400

    Rewards button

--- StudyBuddyApp/StudyBuddyApp/Views/RewardsView.swift
+++ StudyBuddyApp/StudyBuddyApp/Views/RewardsView.swift
@@ -12,7 +12,29 @@ struct RewardsView: View {
     @EnvironmentObject var viewRouter: ViewRouter
     
     var body: some View {
-        Text("Rewards View")
+        VStack {
+            Text("Nicely Done!")
+                .font(Font.custom("Chalkboard SE", size: 40))
+            Button(action: { viewRouter.currentPage = .tabbedPage }) {
+                Image(systemName: "gift.fill")
+                    .font(Font.custom("Chalkboard SE", size: 35))
+                // TODO: Calculate final rewards
+//                Text("\(Task.calculateBaseRewards(duration: viewModel.currentTask!.duration))+")
+                Text("+150")
+                    .font(Font.custom("Chalkboard SE", size: 24))
+                    .baselineOffset(5)
+                    .padding(.trailing, 10)
+                Image("coin")
+                    .resizable()
+                    .frame(width: 32.0, height: 32.0)
+            }
+            .padding(.vertical, 10)
+            .padding(.horizontal, 35)
+            .background(Color(red: 142 / 255, green: 248 / 255, blue: 116 / 255))
+            .foregroundColor(.black)
+            .cornerRadius(10)
+            .shadow(color: Color.gray, radius: 3, x: 0, y: 5)
+        }
     }
 }
 
