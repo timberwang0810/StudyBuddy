ad9dde774a2600c81413bf92c62812544458aebf
Austin Leung <austinle@andrew.cmu.edu>
2021-11-15 21:44:54 -0500

    Add scroll view

--- StudyBuddyApp/StudyBuddyApp/Views/PlaygroundView.swift
+++ StudyBuddyApp/StudyBuddyApp/Views/PlaygroundView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 import SpriteKit
 
 struct PlaygroundView: View {
-  @ObservedObject var viewModel: ViewModel
-  @EnvironmentObject var viewRouter: ViewRouter
+    @ObservedObject var viewModel: ViewModel
+    @EnvironmentObject var viewRouter: ViewRouter
     
     var scene: SKScene {
         let scene = PlaygroundScene()
@@ -20,30 +20,48 @@ struct PlaygroundView: View {
     }
     
     var body: some View {
-      VStack{
-        HStack{
-          Text("\(viewModel.getCurrentMoney())")
-              .font(Font.custom("Chalkboard SE", size: 24))
-              .baselineOffset(5)
-              .padding(.trailing, 10)
-            .onAppear(perform: {
-              self.viewModel.updateUserData()
-            })
-          Image("coin")
-              .resizable()
-              .frame(width: 32.0, height: 32.0)
-          
+        ZStack{
+            VStack {
+                Spacer()
+                Divider()
+                ScrollView(.horizontal) {
+                    HStack(spacing: 10) {
+                        ForEach(0..<10) { index in
+                            Circle()
+                               .fill(Color.yellow)
+                               .frame(width: 70, height: 70)
+                           Text("\(index)")
+                        }
+                    }.padding()
+                    .padding(.top, 20)
+                }.frame(height: 100)
+                Divider()
+                
+                HStack{
+                    Text("\(viewModel.getCurrentMoney())")
+                        .font(Font.custom("Chalkboard SE", size: 24))
+                        .baselineOffset(5)
+                        .padding(.trailing, 10)
+                        .onAppear(perform: {
+                            self.viewModel.updateUserData()
+                        })
+                    Image("coin")
+                        .resizable()
+                        .frame(width: 32.0, height: 32.0)
+                    
+                }
+                
+                SpriteView(scene: scene)
+                    .frame(width: 400.0, height: 700.0)
+                    .edgesIgnoringSafeArea(.all)
+            }
         }
-        SpriteView(scene: scene)
-            .frame(width: 400.0, height: 700.0)
-            .edgesIgnoringSafeArea(.all)
-      }
-      
+        
     }
 }
 
 struct PlaygroundView_Previews: PreviewProvider {
     static var previews: some View {
-      PlaygroundView(viewModel: ViewModel()).environmentObject(ViewRouter())
+        PlaygroundView(viewModel: ViewModel()).environmentObject(ViewRouter())
     }
 }
