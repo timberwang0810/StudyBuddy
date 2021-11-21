f799a79655a4bf7ffc5abb6997762be0ef5b3218
Austin Leung <austinle@andrew.cmu.edu>
2021-11-21 11:26:01 -0500

    Toggle menu with button

--- StudyBuddyApp/StudyBuddyApp/Views/PlaygroundView.swift
+++ StudyBuddyApp/StudyBuddyApp/Views/PlaygroundView.swift
@@ -12,6 +12,7 @@ struct PlaygroundView: View {
   @ObservedObject var viewModel: ViewModel
   @EnvironmentObject var viewRouter: ViewRouter
   @State private var selectedItems: [PlaygroundItem: Bool] = [:]
+  @State private var showMenu: Bool = false
   
   let MENU_BG_COLOR = Color(red: 248 / 255, green: 208 / 255, blue: 116 / 255)
   
@@ -38,43 +39,55 @@ struct PlaygroundView: View {
     ZStack{
       VStack {
         Spacer()
-        Divider()
-        ScrollView(.horizontal) {
-          HStack(spacing: 0) {
-            ForEach(viewModel.getAllPlaygroundItems(), id: \.self) { playgroundItem in
-              PlaygroundItemView(viewModel: viewModel, item: playgroundItem, isInUse: selectedItems[playgroundItem] ?? false)
-                .onTapGesture {
-                  print(playgroundItem.name)
-                  print("Before: \(viewModel.isItemInUse(item: playgroundItem))")
-                  viewModel.togglePlaygroundItem(item: playgroundItem)
-                  // update visual display of whether item is selected
-                  for item in viewModel.getAllPlaygroundItems() {
-                    self.selectedItems[item] = viewModel.isItemInUse(item: item)
-                  }
-                  
-                  print("After: \(viewModel.isItemInUse(item: playgroundItem))")
-                }
-            }
-          }.padding(.horizontal, 15)
-        }.frame(height: 93)
-        .background(MENU_BG_COLOR)
-        .overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color.gray))
-        .padding(.horizontal, 20)
-        Divider()
-        
-        HStack{
-          Text("\(viewModel.getCurrentMoney())")
-            .font(Font.custom("Chalkboard SE", size: 24))
-            .baselineOffset(5)
-            .padding(.trailing, 10)
-            .onAppear(perform: {
-              self.viewModel.updateUserData()
-            })
-          Image("coin")
-            .resizable()
-            .frame(width: 32.0, height: 32.0)
-          
+        Button(action: {
+          self.showMenu.toggle()
+            }) {
+            Text("Menu")
+                .frame(width: 100, height: 100)
+                .foregroundColor(Color.black)
+                .background(Color.red)
+                .clipShape(Circle())
         }
+        
+        if self.showMenu {
+          Divider()
+          ScrollView(.horizontal) {
+            HStack(spacing: 0) {
+              ForEach(viewModel.getAllPlaygroundItems(), id: \.self) { playgroundItem in
+                PlaygroundItemView(viewModel: viewModel, item: playgroundItem, isInUse: selectedItems[playgroundItem] ?? false)
+                  .onTapGesture {
+                    print(playgroundItem.name)
+                    print("Before: \(viewModel.isItemInUse(item: playgroundItem))")
+                    viewModel.togglePlaygroundItem(item: playgroundItem)
+                    // update visual display of whether item is selected
+                    for item in viewModel.getAllPlaygroundItems() {
+                      self.selectedItems[item] = viewModel.isItemInUse(item: item)
+                    }
+                    
+                    print("After: \(viewModel.isItemInUse(item: playgroundItem))")
+                  }
+              }
+            }.padding(.horizontal, 15)
+          }.frame(height: 93)
+          .background(MENU_BG_COLOR)
+          .overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color.gray))
+          .padding(.horizontal, 20)
+          Divider()
+        }
+        
+//        HStack{
+//          Text("\(viewModel.getCurrentMoney())")
+//            .font(Font.custom("Chalkboard SE", size: 24))
+//            .baselineOffset(5)
+//            .padding(.trailing, 10)
+//            .onAppear(perform: {
+//              self.viewModel.updateUserData()
+//            })
+//          Image("coin")
+//            .resizable()
+//            .frame(width: 32.0, height: 32.0)
+//
+//        }
         
         SpriteView(scene: scene)
           .frame(width: 400.0, height: 700.0)
