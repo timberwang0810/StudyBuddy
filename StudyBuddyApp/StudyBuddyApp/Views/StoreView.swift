f759dadb33f6fec11d0e7c53b38c0a74b9c09df6
Austin Leung <austinle@andrew.cmu.edu>
2021-11-26 13:35:54 -0500

    Add background for entire view and coin

--- StudyBuddyApp/StudyBuddyApp/Views/StoreView.swift
+++ StudyBuddyApp/StudyBuddyApp/Views/StoreView.swift
@@ -11,26 +11,28 @@ struct StoreView: View {
   @ObservedObject var viewModel: ViewModel
   @EnvironmentObject var viewRouter: ViewRouter
   let data = (1...10).map { "Item \($0)" }
-
+  
   let columns = [
-      GridItem(.adaptive(minimum: 80))
+    GridItem(.adaptive(minimum: 80))
   ]
-//  ForEach(viewModel.getStorageItems(), id: \.self) { playgroundItem in
-//    PlaygroundItemView(viewModel: viewModel, item: playgroundItem)
-//      .onTapGesture {
-//        print(playgroundItem.name)
-//        print("Before: \(viewModel.isItemInUse(item: playgroundItem))")
-//        viewModel.togglePlaygroundItem(item: playgroundItem)
-//        print("After: \(viewModel.isItemInUse(item: playgroundItem))")
-//
+  //  ForEach(viewModel.getStorageItems(), id: \.self) { playgroundItem in
+  //    PlaygroundItemView(viewModel: viewModel, item: playgroundItem)
+  //      .onTapGesture {
+  //        print(playgroundItem.name)
+  //        print("Before: \(viewModel.isItemInUse(item: playgroundItem))")
+  //        viewModel.togglePlaygroundItem(item: playgroundItem)
+  //        print("After: \(viewModel.isItemInUse(item: playgroundItem))")
+  //
   var body: some View {
-       ScrollView {
-        VStack(spacing: 5) {
+    ZStack {
+      Color(red: 241 / 255, green: 241 / 255, blue: 241 / 255).edgesIgnoringSafeArea([.top])
+      ScrollView {
+        VStack(spacing: 8) {
           Text("Store").font(Font.custom("Chalkboard SE", size: 30))
           HStack{
             Image("coin")
               .resizable()
-              .frame(width: 14.0, height: 14.0)
+              .frame(width: 20.0, height: 20.0)
             Text("\(viewModel.getCurrentMoney())")
               .font(Font.custom("Chalkboard SE", size: 24))
               .baselineOffset(5)
@@ -39,43 +41,46 @@ struct StoreView: View {
                 self.viewModel.updateUserData()
               })
           }
+          .padding(.horizontal, 30)
+          .background( RoundedRectangle(cornerRadius: 8).fill(Color.white))
         }
         LazyVGrid(columns: Array(repeating: GridItem(), count: 2), spacing: 20) {
-               ForEach(viewModel.getStoreItems(), id: \.self) { storeItem in
-               StoreItemView(viewModel: viewModel,  item:  storeItem)
-                      .onTapGesture {
-                        print(storeItem.name)
-               }
-           }
-       }
+          ForEach(viewModel.getStoreItems(), id: \.self) { storeItem in
+            StoreItemView(viewModel: viewModel,  item:  storeItem)
+              .onTapGesture {
+                print(storeItem.name)
+              }
+          }
+        }
+      }
     }
-}
-
-struct StoreView_Previews: PreviewProvider {
+  }
+  
+  struct StoreView_Previews: PreviewProvider {
     static var previews: some View {
       StoreView(viewModel: ViewModel()).environmentObject(ViewRouter())
     }
-}
+  }
   
-
-//VStack(alignment: .leading, spacing: 10) {
-//   Image("\(item.image)")
-//           .resizable()
-//           .frame(width: 150, height: 200)
-//           .padding(4)
-//           .cornerRadius(10)
-//
-//   Text(item.name)
-//   Text(item.price)
-
-//
-//var body: some View {
-//         ScrollView {
-//             LazyVGrid(columns: Array(repeating: GridItem(), count: 4)) {
-//                 ForEach(data, id: \.self) { object in
-//                     StoreItem(item: object)
-//                 }
-//             }
-//         }
-//    }
+  
+  //VStack(alignment: .leading, spacing: 10) {
+  //   Image("\(item.image)")
+  //           .resizable()
+  //           .frame(width: 150, height: 200)
+  //           .padding(4)
+  //           .cornerRadius(10)
+  //
+  //   Text(item.name)
+  //   Text(item.price)
+  
+  //
+  //var body: some View {
+  //         ScrollView {
+  //             LazyVGrid(columns: Array(repeating: GridItem(), count: 4)) {
+  //                 ForEach(data, id: \.self) { object in
+  //                     StoreItem(item: object)
+  //                 }
+  //             }
+  //         }
+  //    }
 }
