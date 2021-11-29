f18bbbc111a40eb3cce69a13e40633d37c172a62
timberwang0810 <zilinwan@andrew.cmu.edu>
2021-11-29 16:02:07 -0500

    added push notification for when user leaves app in bg

--- StudyBuddyApp/StudyBuddyApp/AppDelegate.swift
+++ StudyBuddyApp/StudyBuddyApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+        if success {
+            print("All set!")
+        } else if let error = error {
+            print(error.localizedDescription)
+        }
+    }
     return true
   }
 
